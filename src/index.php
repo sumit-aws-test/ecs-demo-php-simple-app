124644af12a37ad3f6f55fdece62a06f5f9ff15f
sumit-aws-test <47230271+sumit-aws-test@users.noreply.github.com>
2019-02-01 11:09:18 +1100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Sumit AWS Test CI/CD</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations - approval process added</h2>
                 <p>Your Telstra application is now running on a Docker container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
